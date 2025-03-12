442729b70bdf25e4d122d0330131fe5ec29e5dd3
Alberte Mozo <albertegz@gmail.com>
2025-03-12 19:29:28 +0100

    Add a failing test

--- tests/BarShould.php
+++ tests/BarShould.php
@@ -7,11 +7,17 @@ namespace Tests;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
-class GreenBarShould extends TestCase
+class BarShould extends TestCase
 {
     #[Test]
     public function beGreen(): void
     {
         $this->assertTrue(true);
+    }
+
+    #[Test]
+    public function beRed(): void
+    {
+        $this->assertTrue(false);
     }
 }
