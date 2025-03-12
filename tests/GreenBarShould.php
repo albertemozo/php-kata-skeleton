5f92c7f7a728129b7ce4b179bed06843702f3c40
Alberte Mozo <albertegz@gmail.com>
2025-03-12 19:27:54 +0100

    Upgrade PHPUnit

--- tests/GreenBarShould.php
+++ tests/GreenBarShould.php
@@ -4,13 +4,12 @@ declare(strict_types=1);
 
 namespace Tests;
 
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 
 class GreenBarShould extends TestCase
 {
-    /**
-     * @test
-     */
+    #[Test]
     public function beGreen(): void
     {
         $this->assertTrue(true);
